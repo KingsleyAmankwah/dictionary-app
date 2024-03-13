e72583f0bab5cb5eb7e39573e730ef298c56b7b5
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-03-13 09:05:05 +0000

    feat(app): search word functionality added

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { AppService } from '../services/app.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+  public word: string = '';
+  public results: string = '';
 
+  constructor(private appService: AppService) {}
+
+  public searchWord() {
+    this.appService.searchWord(this.word).subscribe(
+      (response) => {
+        this.results = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log('Error fetching', error);
+      }
+    );
+  }
 }
