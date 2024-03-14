ee16df30f80cb571450bad0ad61692234902341d
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-03-14 17:52:23 +0000

    light and dark theme functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ContentComponent } from './content/content.component';
+import { Word } from './interface';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
@@ -20,4 +22,28 @@ import { ContentComponent } from './content/content.component';
 })
 export class AppComponent {
   title = 'dictionary-app';
+  public results: Word[] = [];
+  isDarkTheme = false;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private appService: AppService
+  ) {}
+
+  applyTheme() {
+    if (this.isDarkTheme) {
+      this.document.body.style.backgroundColor = '#000';
+      this.document.body.style.color = '#fff';
+    } else {
+      this.document.body.style.backgroundColor = '#fff';
+      this.document.body.style.color = '#000';
+    }
+  }
+
+  ngOnInit() {
+    this.appService.isDarkTheme.subscribe((darkTheme) => {
+      this.isDarkTheme = darkTheme;
+      this.applyTheme();
+    });
+  }
 }
