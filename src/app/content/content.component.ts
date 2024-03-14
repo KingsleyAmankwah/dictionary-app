aad33608d31c962619ec12456d1b1aa44cbe44d6
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-03-14 17:54:33 +0000

    content

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Word } from '../interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrl: './content.component.css',
 })
 export class ContentComponent {
+  @Input() public results: Word[] = [];
 
+  playAudio(audioSrc: string) {
+    const audioElement = document.createElement('audio');
+    audioElement.src = audioSrc;
+    audioElement.play();
+  }
+
+  // public objectKeys(obj: { [key: string]: {} }): string[] {
+  //   if (obj) {
+  //     return Object.keys(obj);
+  //   } else {
+  //     return [];
+  //   }
+  // }
 }
