6f97f9e8204fdea48c90d541b3af855387cc6cc4
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-03-14 17:51:53 +0000

    light and dark theme feature implemented

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environment/environment';
 import { HttpClient } from '@angular/common/http';
+import { Word } from '../interface';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +11,17 @@ export class AppService {
   private API_URL = environment.BASE_URL;
   constructor(private http: HttpClient) {}
 
+  private isDarkThemeSubject = new BehaviorSubject<boolean>(false);
+  isDarkTheme = this.isDarkThemeSubject.asObservable();
+
+  toggleTheme() {
+    const currentTheme = this.isDarkThemeSubject.value;
+    this.isDarkThemeSubject.next(!currentTheme);
+  }
+
   searchWord(word: string) {
     const url = `${this.API_URL}${word}`;
     console.log(url);
-    return this.http.get<string>(url);
+    return this.http.get<Word[]>(url);
   }
 }
