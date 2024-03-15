e2982a8683e668956e8eaa0206f4adeb3d7612fc
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-03-15 16:01:31 +0000

    refactoring

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -9,19 +9,25 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class AppService {
   private API_URL = environment.BASE_URL;
+  private isDarkThemeSubject = new BehaviorSubject<boolean>(false);
+  private fontFamilySubject = new BehaviorSubject<string>('Sans-serif');
+
+  public isDarkTheme = this.isDarkThemeSubject.asObservable();
+  public currentFont = this.fontFamilySubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
-  private isDarkThemeSubject = new BehaviorSubject<boolean>(false);
-  isDarkTheme = this.isDarkThemeSubject.asObservable();
-
-  toggleTheme() {
+  public toggleTheme() {
     const currentTheme = this.isDarkThemeSubject.value;
     this.isDarkThemeSubject.next(!currentTheme);
   }
 
-  searchWord(word: string) {
+  public setFontFamily(fontFamily: string) {
+    this.fontFamilySubject.next(fontFamily);
+  }
+
+  public searchWord(word: string) {
     const url = `${this.API_URL}${word}`;
-    console.log(url);
     return this.http.get<Word[]>(url);
   }
 }
